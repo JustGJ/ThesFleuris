2a7c5a446c217d642591af14c577c734776d8980
Jeff <jf.gasp@gmail.com>
2021-02-25 13:52:05 +1100

    commit gh

---
+++ build/precache-manifest.d5d566b9872cde27fccd17891122f0bf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219fbb0e2469332849111a5fd901689b",
+    "url": "/ThesFleuris/index.html"
+  },
+  {
+    "revision": "9166bedf517680ec1e7f",
+    "url": "/ThesFleuris/static/css/main.88370e72.chunk.css"
+  },
+  {
+    "revision": "62ba6128bd698a3a76bb",
+    "url": "/ThesFleuris/static/js/2.ec49f7a8.chunk.js"
+  },
+  {
+    "revision": "ac27d415db6a8f4ccefe45d91d40e399",
+    "url": "/ThesFleuris/static/js/2.ec49f7a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9166bedf517680ec1e7f",
+    "url": "/ThesFleuris/static/js/main.5d403175.chunk.js"
+  },
+  {
+    "revision": "f5b4ffa1927fe97b84ba",
+    "url": "/ThesFleuris/static/js/runtime-main.af762313.js"
+  },
+  {
+    "revision": "249f92f85a55312f4231f26895fa992b",
+    "url": "/ThesFleuris/static/media/Tea.249f92f8.jpg"
+  },
+  {
+    "revision": "791ef8ea847066b8842d4903299dd803",
+    "url": "/ThesFleuris/static/media/accessories.791ef8ea.jpg"
+  },
+  {
+    "revision": "3cda4c23e893dc30be7db87f0b3abcac",
+    "url": "/ThesFleuris/static/media/blog.3cda4c23.jpg"
+  },
+  {
+    "revision": "84f78f810cf743082242c5b6cd0f3bf4",
+    "url": "/ThesFleuris/static/media/camomille.84f78f81.png"
+  },
+  {
+    "revision": "d7726ee3d41ceccfb46a344f9b81d5a0",
+    "url": "/ThesFleuris/static/media/contact.d7726ee3.jpg"
+  },
+  {
+    "revision": "ffb42a96f270223e5f61e5e0de2750d2",
+    "url": "/ThesFleuris/static/media/earlGrey.ffb42a96.png"
+  },
+  {
+    "revision": "496253e3db853ca675617b14da83a617",
+    "url": "/ThesFleuris/static/media/home.496253e3.jpg"
+  },
+  {
+    "revision": "9c8a4463accaa5ce6959800a28436828",
+    "url": "/ThesFleuris/static/media/romarin.9c8a4463.png"
+  }
+]);
