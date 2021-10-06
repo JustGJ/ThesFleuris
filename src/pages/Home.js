51449d67680d43cf1aad00926be9f8499afaf62c
Jeff <jf.gasp@gmail.com>
2021-10-06 23:22:20 +0200

    opacity fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,19 @@ import News from '../components/News/News';
 import Opinion from '../components/Opinion/Opinion';
 
 import home from '../assets/header/home.jpg';
-import "../App.css";
+import '../App.css';
 
 const Home = ({ isOpen, addCart, setCart, cart }) => {
-
-
     return (
-        <div className={`${cart && "backgroundOpacity"}`} onClick={() => setCart(false)}>
-            <Header background={home} title="C'est toujours l'heure du thé grâce aux tisanes fleuries"  />
+        <div onClick={() => setCart(false)}>
+            <Header
+                background={home}
+                title="C'est toujours l'heure du thé grâce aux tisanes fleuries"
+            />
             <News addCart={addCart} isOpen={isOpen} cart={cart} setCart={setCart} />
             <Opinion />
         </div>
-    )
-}
+    );
+};
 
 export default Home;
